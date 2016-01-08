9f92ff2bf3019e23a29e6e44511834fadde14bd0
Will Hindenburg <WillH1983@yahoo.com>
2016-01-07 22:21:32 -0600

    Added retry block error.

--- Pod/Source Files/ViewControllers/BaseTableViewController.swift
+++ Pod/Source Files/ViewControllers/BaseTableViewController.swift
@@ -87,5 +87,51 @@ public class BaseTableViewController: UITableViewController {
         
         self.presentViewController(alertController, animated: true, completion: nil)
     }
+    
+    public func showError(error:NSError, retryBlock:(Void -> Void)) {
+        
+    }
+    
+    /*
+    - (void)showError:(NSError *)error withRetryBlock:(void (^)(void))completion {
+    if ([User persistentUserObject].sessionToken) {
+    NSString *errorMessage = [error localizedDescription];
+    NSString *title;
+    UIAlertControllerStyle style = UIAlertControllerStyleActionSheet;
+    
+    if (error.code == ServiceErrorForceUpgrade) {
+    title = [error.userInfo objectForKey:kForceUpgradeTitleKey];
+    style = UIAlertControllerStyleAlert;
+    } else if ([error code] == ServiceErrorMaintenanceMode) {
+    title = [error.userInfo objectForKey:kMaintenanceModeErrorTitleKey];
+    }
+    
+    if (title == nil) {
+    title = @"Error";
+    }
+    
+    UIAlertController *errorAlert = [UIAlertController alertControllerWithTitle:title message:errorMessage preferredStyle:style];
+    
+    if (error.code != ServiceErrorForceUpgrade) {
+    [errorAlert addAction:[UIAlertAction actionWithTitle:@"Retry" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+    if (completion) completion();
+    }]];
+    } else {
+    NSString *url = [error.userInfo objectForKey:kForceUpgradeURL];
+    if (url) {
+    [errorAlert addAction:[UIAlertAction actionWithTitle:@"Upgrade" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
+    
+    }]];
+    }
+    
+    }
+    
+    [errorAlert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+    [self presentViewController:errorAlert animated:YES completion:nil];
+    }
+    
+    }
+*/
 
 }
