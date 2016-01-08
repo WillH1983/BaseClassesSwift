22b27647010b8b260f49c9a34ed17275e11c51a0
Will Hindenburg <WillH1983@yahoo.com>
2016-01-07 22:31:20 -0600

    Added service errors, and finished show error with retry.

--- Pod/Source Files/ViewControllers/BaseTableViewController.swift
+++ Pod/Source Files/ViewControllers/BaseTableViewController.swift
@@ -89,49 +89,40 @@ public class BaseTableViewController: UITableViewController {
     }
     
     public func showError(error:NSError, retryBlock:(Void -> Void)) {
-        
+        if User.persistentUserObject().sessionToken.isEmpty == false {
+            let errorMessage = error.localizedDescription
+            var title = ""
+            var style = UIAlertControllerStyle.ActionSheet
+            
+            if error.code == SwiftServiceErrorCode.ForceUpgrade.rawValue {
+                title = error.userInfo[kForceUpgradeTitleKey] as? String ?? "Error"
+                style = UIAlertControllerStyle.Alert
+                
+            } else if error.code == SwiftServiceErrorCode.MaintenanceMode.rawValue {
+                title = error.userInfo[kMaintenanceModeErrorTitleKey] as? String ?? "Error"
+            }
+            
+            let errorAlert = UIAlertController(title: title, message: errorMessage, preferredStyle: style)
+            
+            if error.code != SwiftServiceErrorCode.ForceUpgrade.rawValue {
+                errorAlert.addAction(UIAlertAction(title: "Retry", style: UIAlertActionStyle.Destructive, handler: { (action) -> Void in
+                    retryBlock()
+                }))
+            } else {
+                if let url = error.userInfo[kForceUpgradeURL] as? String {
+                    errorAlert.addAction(UIAlertAction(title: "Upgrade", style: UIAlertActionStyle.Destructive, handler: { (action) -> Void in
+                        let actualURL = NSURL(string: url)
+                        if (actualURL != nil) {
+                            UIApplication.sharedApplication().openURL(actualURL!)
+                        }
+                        
+                    }))
+                }
+            }
+            errorAlert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
+            
+            
+        }
     }
-    
-    /*
-    - (void)showError:(NSError *)error withRetryBlock:(void (^)(void))completion {
-    if ([User persistentUserObject].sessionToken) {
-    NSString *errorMessage = [error localizedDescription];
-    NSString *title;
-    UIAlertControllerStyle style = UIAlertControllerStyleActionSheet;
-    
-    if (error.code == ServiceErrorForceUpgrade) {
-    title = [error.userInfo objectForKey:kForceUpgradeTitleKey];
-    style = UIAlertControllerStyleAlert;
-    } else if ([error code] == ServiceErrorMaintenanceMode) {
-    title = [error.userInfo objectForKey:kMaintenanceModeErrorTitleKey];
-    }
-    
-    if (title == nil) {
-    title = @"Error";
-    }
-    
-    UIAlertController *errorAlert = [UIAlertController alertControllerWithTitle:title message:errorMessage preferredStyle:style];
-    
-    if (error.code != ServiceErrorForceUpgrade) {
-    [errorAlert addAction:[UIAlertAction actionWithTitle:@"Retry" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
-    if (completion) completion();
-    }]];
-    } else {
-    NSString *url = [error.userInfo objectForKey:kForceUpgradeURL];
-    if (url) {
-    [errorAlert addAction:[UIAlertAction actionWithTitle:@"Upgrade" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
-    
-    }]];
-    }
-    
-    }
-    
-    [errorAlert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
-    [self presentViewController:errorAlert animated:YES completion:nil];
-    }
-    
-    }
-*/
 
 }
